f596a1b57b90b4d3937775ce853d059be52edde3
Daniel Klarenbach <daniel.klarenbach@gmail.com>
2021-03-02 20:29:41 +0100

    refactor: Changed create_schema.py - removed ok return parameter from mutations

--- api/create_schema.py
+++ api/create_schema.py
@@ -13,7 +13,6 @@ from api.tasks import check_for_matching_application
 
 
 class CreateUniversityAdmin(graphene.Mutation):
-    ok = graphene.Boolean()
     university_admin = graphene.Field(UniversityAdminNode)
 
     class Arguments:
@@ -30,11 +29,10 @@ class CreateUniversityAdmin(graphene.Mutation):
         university_admin_user.save()
         university_admin = UniversityAdmin.objects.create(user=university_admin_user)
         University(name=university_name, university_admin=university_admin).save()
-        return CreateUniversityAdmin(ok=True, university_admin=university_admin)
+        return CreateUniversityAdmin(university_admin=university_admin)
 
 
 class CreateDepartment(graphene.Mutation):
-    ok = graphene.Boolean()
     department = graphene.Field(DepartmentNode)
 
     class Arguments:
@@ -47,11 +45,10 @@ class CreateDepartment(graphene.Mutation):
         university_admin_user = info.context.user
         university = university_admin_user.university_admin.university
         department = Department.objects.create(university=university, name=name)
-        return CreateDepartment(ok=True, department=department)
+        return CreateDepartment(department=department)
 
 
 class CreateDepartmentAdmin(graphene.Mutation):
-    ok = graphene.Boolean()
     department_admin = graphene.Field(DepartmentAdminNode)
 
     class Arguments:
@@ -70,11 +67,10 @@ class CreateDepartmentAdmin(graphene.Mutation):
         department_admin_user.save()
         department_admin = DepartmentAdmin.objects.create(user=department_admin_user,
                                                           department_id=department_id)
-        return CreateDepartmentAdmin(ok=True, department_admin=department_admin)
+        return CreateDepartmentAdmin(department_admin=department_admin)
 
 
 class CreateYear(graphene.Mutation):
-    ok = graphene.Boolean()
     year = graphene.Field(YearNode)
 
     class Arguments:
@@ -87,11 +83,10 @@ class CreateYear(graphene.Mutation):
         department_admin_user = info.context.user
         department = department_admin_user.department_admin.department
         year = Year.objects.create(start_year=start_year, department=department)
-        return CreateYear(ok=True, year=year)
+        return CreateYear(year=year)
 
 
 class CreateFieldOfStudy(graphene.Mutation):
-    ok = graphene.Boolean()
     field_of_study = graphene.Field(FieldOfStudyNode)
 
     class Arguments:
@@ -103,10 +98,9 @@ class CreateFieldOfStudy(graphene.Mutation):
     @is_objects_department_admin(model=Year, id_kwarg='year_id')
     def mutate(cls, root, info, year_id, name):
         field_of_study = FieldOfStudy.objects.create(name=name, year_id=year_id)
-        return CreateFieldOfStudy(ok=True, field_of_study=field_of_study)
+        return CreateFieldOfStudy(field_of_study=field_of_study)
 
 class CreateSubjectType(graphene.Mutation):
-    ok = graphene.Boolean()
     subject_type = graphene.Field(SubjectTypeNode)
 
     class Arguments:
@@ -118,11 +112,10 @@ class CreateSubjectType(graphene.Mutation):
     @is_objects_department_admin(model=FieldOfStudy, lookup='year__department', id_kwarg='field_of_study_id')
     def mutate(cls, root, info,  field_of_study_id, name):
         subject_type=SubjectType.objects.create(field_of_study_id=field_of_study_id,name=name)
-        return CreateSubjectType(ok=True,subject_type=subject_type)
+        return CreateSubjectType(subject_type=subject_type)
 
 
 class CreateSubject(graphene.Mutation):
-    ok = graphene.Boolean()
     subject = graphene.Field(SubjectNode)
 
     class Arguments:
@@ -142,11 +135,10 @@ class CreateSubject(graphene.Mutation):
         subject = Subject.objects.create(subject_type_id=subject_type_id, description=description,
                                          lecturer=lecturer, day=day, type=type, start_time=start_time,
                                          end_time=end_time, limit=limit)
-        return CreateSubject(ok=True, subject=subject)
+        return CreateSubject(subject=subject)
 
 
 class CreateStudent(graphene.Mutation):
-    ok = graphene.Boolean()
     student = graphene.Field(StudentNode)
 
     class Arguments:
@@ -164,11 +156,10 @@ class CreateStudent(graphene.Mutation):
         student_user.set_password(password)
         student_user.save()
         student = Student.objects.create(user=student_user, field_of_study_id=field_of_study_id)
-        return CreateStudent(ok=True, student=student)
+        return CreateStudent(student=student)
 
 
 class CreateSubjectGroup(graphene.Mutation):
-    ok = graphene.Boolean()
     subject_group = graphene.Field(SubjectGroupNode)
 
     class Arguments:
@@ -181,11 +172,10 @@ class CreateSubjectGroup(graphene.Mutation):
     @is_objects_department_admin(model=Subject, lookup='subject_type__field_of_study__year__department', id_kwarg='subject_id')
     def mutate(cls, root, info, subject_id, student_id):
         subject_group = SubjectGroup.objects.create(subject_id=subject_id, student_id=student_id)
-        return CreateSubjectGroup(ok=True, subject_group=subject_group)
+        return CreateSubjectGroup(subject_group=subject_group)
 
 
 class CreatePoints(graphene.Mutation):
-    ok = graphene.Boolean()
     points = graphene.Field(PointsNode)
 
     class Arguments:
@@ -199,11 +189,10 @@ class CreatePoints(graphene.Mutation):
     @is_owner(model=Student, id_kwarg='student_id')
     def mutate(cls, root, info, subject_id, student_id, points):
         points = Points.objects.create(subject_id=subject_id, student_id=student_id, points=points)
-        return CreatePoints(ok=True, points=points)
+        return CreatePoints(points=points)
 
 
 class CreateApplication(graphene.Mutation):
-    ok = graphene.Boolean()
     application = graphene.Field(ApplicationNode)
 
     class Arguments:
@@ -221,7 +210,7 @@ class CreateApplication(graphene.Mutation):
                                                  wanted_subject_id=wanted_subject_id,
                                                  student_id=student_id)
         #check_for_matching_application.delay()
-        return CreateApplication(ok=True, application=application)
+        return CreateApplication(application=application)
 
 
 class Mutation(graphene.ObjectType):
