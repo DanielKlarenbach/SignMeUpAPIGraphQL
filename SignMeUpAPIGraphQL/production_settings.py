4032de2088c6f80c91baac8ec6a9111eb8f5f8ea
DanielKlarenbach <62997153+DanielKlarenbach@users.noreply.github.com>
2021-03-05 15:12:16 +0100

    Update production_settings.py

--- SignMeUpAPIGraphQL/production_settings.py
+++ SignMeUpAPIGraphQL/production_settings.py
@@ -85,7 +85,6 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'SMU2',
         'USER': 'postgres',
-        'PASSWORD': 'ebee407f18b2bea49dc430d6acfb03c2',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
